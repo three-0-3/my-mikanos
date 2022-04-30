1a82473dd001a16e37499e2ce3329248ac5d50ac
three <moresuke0@gmail.com>
2022-04-30 14:36:22 +0000

    Add Mouse class

--- kernel/mouse.cpp
+++ kernel/mouse.cpp
@@ -48,40 +48,41 @@ void DrawMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position) {
   }
 }
 
-// mouse layer id defined here to be used in MouseObserver
-unsigned int mouse_layer_id;
-Vector2D<int> mouse_position;
+Mouse::Mouse(unsigned int layer_id) : layer_id_{layer_id} {
+}
 
-void MouseObserver(uint8_t buttons, int8_t displacement_x, int8_t displacement_y) {
-  static unsigned int mouse_drag_layer_id = 0;
-  static uint8_t previous_buttons = 0;
+void Mouse::SetPosition(Vector2D<int> position) {
+  position_ = position;
+  layer_manager->Move(layer_id_, position_);
+}
 
-  const auto oldpos = mouse_position;
+void Mouse::OnInterrupt(uint8_t buttons, int8_t displacement_x, int8_t displacement_y) {
+  const auto oldpos = position_;
   // move the mouse cursor window
-  auto newpos = mouse_position + Vector2D<int>{displacement_x, displacement_y};
+  auto newpos = position_ + Vector2D<int>{displacement_x, displacement_y};
   // limit mouse cursor move inside the screen area
-  mouse_position = ElementMax(ElementMin(newpos, ScreenSize() + Vector2D<int>{-1, -1}), {0, 0});
+  position_ = ElementMax(ElementMin(newpos, ScreenSize() + Vector2D<int>{-1, -1}), {0, 0});
 
-  const auto posdiff = mouse_position - oldpos;
+  const auto posdiff = position_ - oldpos;
 
-  layer_manager->Move(mouse_layer_id, mouse_position);
+  layer_manager->Move(layer_id_, position_);
 
-  const bool previous_left_pressed = (previous_buttons & 0x01);
+  const bool previous_left_pressed = (previous_buttons_ & 0x01);
   const bool left_pressed = (buttons & 0x01);
   if (!previous_left_pressed && left_pressed) {
-    auto layer = layer_manager->FindLayerByPosition(mouse_position, mouse_layer_id);
+    auto layer = layer_manager->FindLayerByPosition(position_, layer_id_);
     if (layer && layer->IsDraggable()) {
-      mouse_drag_layer_id = layer->ID();
+      drag_layer_id_ = layer->ID();
     }
   } else if (previous_left_pressed && left_pressed) {
-    if (mouse_drag_layer_id > 0) {
-      layer_manager->MoveRelative(mouse_drag_layer_id, posdiff);
+    if (drag_layer_id_ > 0) {
+      layer_manager->MoveRelative(drag_layer_id_, posdiff);
     }
   } else if (previous_left_pressed && !left_pressed) {
-    mouse_drag_layer_id = 0;
+    drag_layer_id_ = 0;
   }
 
-  previous_buttons = buttons;
+  previous_buttons_ = buttons;
 }
 
 void InitializeMouse() {
@@ -90,14 +91,18 @@ void InitializeMouse() {
   mouse_window->SetTransparentColor(kMouseTransparentColor);
   // save mouse cursor desgin data to mouse_window
   DrawMouseCursor(mouse_window->Writer(), {0, 0});
-  mouse_position = {200, 200};
 
-  mouse_layer_id = layer_manager->NewLayer()
+  auto mouse_layer_id = layer_manager->NewLayer()
     .SetWindow(mouse_window)
-    .Move(mouse_position)
     .ID();
+
+  auto mouse = std::make_shared<Mouse>(mouse_layer_id);
+  mouse->SetPosition({200, 200});
   layer_manager->UpDown(mouse_layer_id, std::numeric_limits<int>::max());
 
 	// set mouse callback method
-  usb::HIDMouseDriver::default_observer = MouseObserver;
+  usb::HIDMouseDriver::default_observer =
+    [mouse](uint8_t buttons, int8_t displacement_x, int8_t displacement_y) {
+      mouse->OnInterrupt(buttons, displacement_x, displacement_y);
+    };
 }
