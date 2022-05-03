dff34dbb1e15f9bc76750a76889e963017296e77
three <moresuke0@gmail.com>
2022-05-03 15:03:32 +0000

    Cursor on the text box

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -70,6 +70,13 @@ void InitializeTextWindow() {
 }
 
 int text_window_index;
+
+void DrawTextCursor(bool visible) {
+  const auto color = visible? ToColor(0) : ToColor(0xffffff);
+  const auto pos = Vector2D<int>{8 + 8 * text_window_index, 24 + 5};
+  FillRectangle(*text_window->Writer(), pos, {7, 15}, color);
+}
+
 void InputTextWindow(char c) {
   if (c == 0) {
     return;
@@ -77,13 +84,17 @@ void InputTextWindow(char c) {
 
   auto pos = []() { return Vector2D<int>{8 + 8 * text_window_index, 24 + 6}; };
 
-  const int max_chars = (text_window->Width() - 16) / 8;
+  const int max_chars = (text_window->Width() - 16) / 8 - 1;
   if (c == '\b' && text_window_index > 0) {
+    DrawTextCursor(false);
     --text_window_index;
     FillRectangle(*text_window->Writer(), pos(), {8, 16}, ToColor(0xffffff));
+    DrawTextCursor(true);
   } else if (c >= ' ' && text_window_index < max_chars) {
+    DrawTextCursor(false);
     WriteAscii(*text_window->Writer(), pos(), c, ToColor(0));
     ++text_window_index;
+    DrawTextCursor(true);
   }
 
   layer_manager->Draw(text_window_layer_id);
@@ -131,6 +142,13 @@ extern "C" void KernelMainNewStack(
 
   InitializeKeyboard(main_queue);
 
+  const int kTextboxCursorTimer = 1;
+  const int kTimer05sec = static_cast<int>(kTimerFreq * 0.5);
+  __asm__("cli");
+  timer_manager->AddTimer(Timer{kTimer05sec, kTextboxCursorTimer});
+  __asm__("sti");
+  bool textbox_cursor_visible = false;
+
   // counter to show on the main window
   char str[128];
 
@@ -167,6 +185,14 @@ extern "C" void KernelMainNewStack(
       usb::xhci::ProcessEvents();
       break;
     case Message::kTimerTimeout:
+      if (msg.arg.timer.value == kTextboxCursorTimer) {
+        __asm__("cli");
+        timer_manager->AddTimer(Timer{msg.arg.timer.timeout + kTimer05sec, kTextboxCursorTimer});
+        __asm__("sti");
+        textbox_cursor_visible = !textbox_cursor_visible;
+        DrawTextCursor(textbox_cursor_visible);
+        layer_manager->Draw(text_window_layer_id);
+      }
       break;
     case Message::kKeyPush:
       InputTextWindow(msg.arg.keyboard.ascii);
