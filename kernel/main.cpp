ef7a26f405f1db4e9c401f64617d39c6f8358327
three <moresuke0@gmail.com>
2022-07-20 11:42:46 +0000

    Remove volume image print

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -155,22 +155,6 @@ extern "C" void KernelMainNewStack(
   InitializeKeyboard();
   InitializeMouse();
 
-  uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
-  printk("Volume Image:\n");
-  for (int i = 0; i < 16; ++i) {
-    printk("%04x:", i * 16);
-    for (int j = 0; j < 8; ++j) {
-      printk(" %02x", *p);
-      ++p;
-    }
-    printk(" ");
-    for (int j = 0; j < 8; ++j) {
-      printk(" %02x", *p);
-      ++p;
-    }
-    printk("\n");
-  }
-
   // counter to show on the main window
   char str[128];
 
