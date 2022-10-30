f48b9d63f0a740f12590c1162ec68f2db4d8274d
three <moresuke0@gmail.com>
2022-10-30 04:45:18 +0000

    Change return type of syscall

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -2,35 +2,41 @@
 
 #include <array>
 #include <cstdint>
+#include <cerrno>
 
 #include "asmfunc.h"
 #include "msr.hpp"
 #include "logger.hpp"
 
 namespace syscall {
+  struct Result {
+    uint64_t value;
+    int error;
+  };
 
 #define SYSCALL(name) \
-  int64_t name( \
+  Result name( \
       uint64_t arg1, uint64_t arg2, uint64_t arg3, \
       uint64_t arg4, uint64_t arg5, uint64_t arg6)
 
 SYSCALL(LogString) {
   if (arg1 != kError && arg1 != kWarn && arg1 != kInfo && arg1 != kDebug) {
-    return -1;
+    return { 0, EPERM };
   }
   const char* s = reinterpret_cast<const char*>(arg2);
-  if (strlen(s) > 1024) {
-    return -1;
+  const auto len = strlen(s);
+  if (len > 1024) {
+    return { 0, E2BIG };
   }
   Log(static_cast<LogLevel>(arg1), "%s", s);
-  return 0;
+  return { len, 0 };
 }
 
 #undef SYSCALL
 
 } // namespace syscall
 
-using SyscallFuncType = int64_t (uint64_t, uint64_t, uint64_t, 
+using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t, 
                                  uint64_t, uint64_t, uint64_t);
 extern "C" std::array<SyscallFuncType*, 1> syscall_table{
   /* 0x00 */ syscall::LogString,
