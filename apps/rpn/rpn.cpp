611876c44c4b1d43c6d98e80bee2d4d11c779010
three <moresuke0@gmail.com>
2022-10-30 05:17:46 +0000

    Remove log from rpn app

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,8 +1,5 @@
 #include <cstring>
 #include <cstdlib>
-#include <cstdint>
-
-#include "../../kernel/logger.hpp"
 
 int stack_ptr;
 long stack[100];
@@ -18,8 +15,6 @@ void Push(long value) {
   stack[stack_ptr] = value;
 }
 
-extern "C" int64_t SyscallLogString(LogLevel, const char*);
-
 extern "C" int main(int argc, char** argv) {
   stack_ptr = -1;
 
@@ -28,22 +23,18 @@ extern "C" int main(int argc, char** argv) {
       long b = Pop();
       long a = Pop();
       Push(a + b);
-      SyscallLogString(kWarn, "+");
     } else if (strcmp(argv[i], "-") == 0) {
       long b = Pop();
       long a = Pop();
       Push(a - b);
-      SyscallLogString(kWarn, "-");
     } else {
       long a = atol(argv[i]);
       Push(a);
-      SyscallLogString(kWarn, "#");
     }
   }
   if (stack_ptr < 0) {
     return 0;
   }
   while (1);
-  SyscallLogString(kWarn, "\nhello, this is rpn\n");
   // return static_cast<int>(Pop());
 }
