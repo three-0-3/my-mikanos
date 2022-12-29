5e5db9894084ff2ed6a839cb3224b5c420f25be9
three <moresuke0@gmail.com>
2022-12-29 07:40:47 +0000

    Quit winhello by Ctrl+Q

--- apps/winhello/winhello.cpp
+++ apps/winhello/winhello.cpp
@@ -1,5 +1,7 @@
 #include "../syscall.h"
 #include <cstdlib>
+#include <cstdio>
+#include <cstring>
 
 extern "C" void main(int argc, char** argv) {
   
@@ -12,6 +14,20 @@ extern "C" void main(int argc, char** argv) {
   SyscallWinWriteString(layer_id, 7, 24, 0xc00000, "hello world!");
   SyscallWinWriteString(layer_id, 24, 40, 0x00c000, "hello world!");
   SyscallWinWriteString(layer_id, 40, 56, 0x0000c0, "hello world!");
+
+  AppEvent events [1];
+  while (true) {
+    auto [ n, err ] = SyscallReadEvent(events, 1);
+    if (err) {
+      printf("ReadEvent failed: %s\n", strerror(err));
+      break;
+    }
+    if (events[0].type == AppEvent::kQuit) {
+      break;
+    } else {
+      printf("unknown event: type = %d\n", events[0].type);
+    }
+  }
 
   SyscallCloseWindow(layer_id);
   exit(0);
