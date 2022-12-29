858d7362cb9969c77946c903aea9e08c8113fde4
three <moresuke0@gmail.com>
2022-12-29 07:33:24 +0000

    Add SyscallReadEvent

--- apps/syscall.h
+++ apps/syscall.h
@@ -9,6 +9,7 @@ extern "C" {
 #endif
 
 #include "../kernel/logger.hpp"
+#include "../kernel/app_event.hpp"
 
 struct SyscallResult {
   uint64_t value;
@@ -28,6 +29,7 @@ struct SyscallResult SyscallWinRedraw(uint64_t layer_id_flags);
 struct SyscallResult SyscallWinDrawLine(uint64_t layer_id_flags, int x0, int y0, int x1, int y1, uint32_t color);
 
 struct SyscallResult SyscallCloseWindow(uint64_t layer_id_flags);
+struct SyscallResult SyscallReadEvent(struct AppEvent* events, size_t len);
 
 #ifdef __cplusplus
 }
--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -13,6 +13,8 @@
 #include "layer.hpp"
 #include "font.hpp"
 #include "timer.hpp"
+#include "keyboard.hpp"
+#include "app_event.hpp"
 
 namespace syscall {
   struct Result {
@@ -74,6 +76,9 @@ SYSCALL(OpenWindow) {
     .Move({x, y})
     .ID();
   active_layer->Activate(layer_id);
+
+  const auto task_id = task_manager->CurrentTask().ID();
+  layer_task_map->insert(std::make_pair(layer_id, task_id));
   __asm__("sti");
 
   return { layer_id, 0 };
@@ -195,9 +200,51 @@ SYSCALL(CloseWindow) {
     active_layer->Activate(0);
     layer_manager->RemoveLayer(layer_id);
     layer_manager->Draw({layer_pos, win_size});
+    layer_task_map->erase(layer_id);
     __asm__("sti");
 
     return { 0, 0 };
+}
+
+SYSCALL(ReadEvent) {
+  if(arg1 < 0x8000'0000'0000'0000) {
+    return { 0, EFAULT};
+  }
+  const auto app_events = reinterpret_cast<AppEvent*>(arg1);
+  const size_t len = arg2;
+
+  __asm__("cli");
+  auto& task = task_manager->CurrentTask();
+  __asm__("sti");
+  size_t i = 0;
+
+  while (i < len) {
+    __asm__("cli");
+    auto msg = task.ReceiveMessage();
+    if (!msg && i == 0) {
+      task.Sleep();
+      continue;
+    }
+    __asm__("sti");
+
+    if (!msg) {
+      break;
+    }
+
+    switch (msg->type) {
+    case Message::kKeyPush:
+      if (msg->arg.keyboard.keycode == 20 /* Q Key */ &&
+          msg->arg.keyboard.modifier & (kLControlBitMask | kRControlBitMask)) {
+        app_events[i].type = AppEvent::kQuit;
+        ++i;
+      }
+      break;    
+    default:
+      Log(kInfo, "uncaught event type: %u\n", msg->type);
+    }
+  }
+
+  return { i, 0 };
 }
 
 #undef SYSCALL
@@ -206,7 +253,7 @@ SYSCALL(CloseWindow) {
 
 using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t, 
                                  uint64_t, uint64_t, uint64_t);
-extern "C" std::array<SyscallFuncType*, 10> syscall_table{
+extern "C" std::array<SyscallFuncType*, 0xb> syscall_table{
   /* 0x00 */ syscall::LogString,
   /* 0x01 */ syscall::PutString,
   /* 0x02 */ syscall::Exit,
@@ -217,6 +264,7 @@ extern "C" std::array<SyscallFuncType*, 10> syscall_table{
   /* 0x07 */ syscall::WinRedraw,
   /* 0x08 */ syscall::WinDrawLine,
   /* 0x09 */ syscall::CloseWindow,
+  /* 0x0a */ syscall::ReadEvent,
 };
 
 void InitializeSyscall() {
