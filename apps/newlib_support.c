efbebf88c03c6dcecf34dd300be36405fcf7f8d8
three <moresuke0@gmail.com>
2023-07-03 10:23:51 +0000

    Add grep app

--- apps/newlib_support.c
+++ apps/newlib_support.c
@@ -41,6 +41,16 @@ int open(const char* path, int flags) {
   return -1;
 }
 
+int posix_memalign(void** memptr, size_t alignment, size_t size) {
+  void* p = malloc(size + alignment - 1);
+  if (!p) {
+    return ENOMEM;
+  }
+  uintptr_t addr = (uintptr_t)p;
+  *memptr = (void*)((addr + alignment - 1) & ~(uintptr_t)(alignment - 1));
+  return 0;
+}
+
 ssize_t read(int fd, void* buf, size_t count) {
   struct SyscallResult res = SyscallReadFile(fd, buf, count);
   if (res.error == 0) {
