ae12e57392d74d4ee8d0363ce2cae2360784138e
three <moresuke0@gmail.com>
2023-07-17 10:04:24 +0000

    Add EOT

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -10,6 +10,7 @@
 #include "paging.hpp"
 #include "asmfunc.h"
 #include "timer.hpp"
+#include "keyboard.hpp"
 
 #include "logger.hpp"
 
@@ -716,10 +717,20 @@ size_t TerminalFileDescriptor::Read(void* buf, size_t len) {
     }
     __asm__("sti");
 
-    if (msg->type == Message::kKeyPush && msg->arg.keyboard.press) {
-      bufc[0] = msg->arg.keyboard.ascii;
-      term_.Print(bufc, 1);
-      return 1;
+    if (msg->type != Message::kKeyPush || msg->arg.keyboard.press) {
+      continue;
     }
+    if (msg->arg.keyboard.modifier & (kLControlBitMask | kRControlBitMask)) {
+      char s[3] = "^ ";
+      s[1] = toupper(msg->arg.keyboard.ascii);
+      term_.Print(s);
+      if (msg->arg.keyboard.keycode == 7 /* D */) {
+        return 0; // EOT
+      }
+      continue;
+    }
+    bufc[0] = msg->arg.keyboard.ascii;
+    term_.Print(bufc, 1);
+    return 1;
   }
 }
