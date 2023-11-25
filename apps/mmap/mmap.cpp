9a4828f1f02ed19df50f779d083be1c8b6dc5628
three <moresuke0@gmail.com>
2023-11-25 05:51:13 +0000

    Add mmap app

---
+++ apps/mmap/mmap.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <cstdio>
+#include <fcntl.h>
+#include "../syscall.h"
+
+extern "C" void main(int argc, char** argv) {
+  SyscallResult res = SyscallOpenFile("/memmap", O_RDONLY);
+  if (res.error) {
+    exit(res.error);
+  }
+  const int fd = res.value;
+  size_t file_size;
+  res = SyscallMapFile(fd, &file_size, 0);
+  if (res.error) {
+    exit(res.error);
+  }
+
+  char* p = reinterpret_cast<char*>(res.value);
+
+  for (size_t i = 0; i < file_size; ++i) {
+    printf("%c", p[i]);
+  }
+  printf("\nres.value (%lx)\n", res.value);
+  printf("\nread from mamped file (%lu bytes)\n", file_size);
+
+  exit(0);
+}
