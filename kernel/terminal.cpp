f1c13fb43b7294bcbb95b0938fba9492d4ca1a81
three <moresuke0@gmail.com>
2024-01-03 05:38:19 +0000

    Fix: stack is too small

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -457,8 +457,9 @@ Error Terminal::ExecuteFile(fat::DirectoryEntry& file_entry, char* command, char
     return argc.error;
   }
 
-  LinearAddress4Level stack_frame_addr{0xffff'ffff'ffff'e000};
-  if (auto err = SetupPageMaps(stack_frame_addr, 1)) {
+  const int stack_size = 8 * 4096;
+  LinearAddress4Level stack_frame_addr{0xffff'ffff'ffff'f000 - stack_size};
+  if (auto err = SetupPageMaps(stack_frame_addr, stack_size / 4096)) {
     return err;
   }
 
@@ -471,10 +472,10 @@ Error Terminal::ExecuteFile(fat::DirectoryEntry& file_entry, char* command, char
     (app_load.vaddr_end + 4096) & 0xffff'ffff'ffff'f000;
   task.SetDPagingBegin(elf_next_page);
   task.SetDPagingEnd(elf_next_page);
-  task.SetFileMapEnd(0xffff'ffff'ffff'e000);
+  task.SetFileMapEnd(stack_frame_addr.value);
 
   int ret = CallApp(argc.value, argv, 3 << 3 | 3, app_load.entry,
-                    stack_frame_addr.value + 4096 - 8,
+                    stack_frame_addr.value + stack_size - 8,
                     &task.OSStackPointer());
 
   task.Files().clear();
